673153b52acd98f7c2ffa583e696ecd2c2504f9c
Wes Adams <wadams1983@gmail.com>
2014-06-13 17:31:42 -0400

    play stuff

---
+++ hello-scala/src/main/scala/Hello.scala
@@ -0,0 +1,16 @@
+
+
+
+
+object Hello {
+  def main(args: Array[String]): Unit = {
+    println("Hello, world!")
+    var l = 'l'
+    printf("%c\n",l);
+    var m = 'm'
+    println(m)
+    val word = "Hello, world!"
+    val space = ' '
+    println(m+space+word+space+l)
+  }
+}
